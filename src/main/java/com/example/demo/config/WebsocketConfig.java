5c94965e0bef9fcb32cfb545979dadb1dd00f301
youngmin <min050410@gmail.com>
2022-08-18 19:34:13 +0900

    ADD :: 채팅 소켓 설정, 소켓 컨트롤러

---
+++ src/main/java/com/example/demo/config/WebsocketConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.example.demo.handler.ChatSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebsocketConfig implements WebSocketConfigurer {
+    private final ChatSocketHandler chatHandler;
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // cors config
+        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+    }
+}
--- src/main/java/com/example/demo/global/config/security/SecurityConfig.java
+++ src/main/java/com/example/demo/global/config/security/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .antMatchers("/ws/chat").permitAll()
                 .antMatchers("/user/**").permitAll()
                 .antMatchers("/auth/logout").authenticated()
                 .antMatchers("/auth/**").permitAll()
