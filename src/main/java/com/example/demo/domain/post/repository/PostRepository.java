e935927583f88e223437d0347ba132344e6cf398
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-08-25 14:19:23 +0900

    FEAT :: PostRepository (#27)

--- src/main/java/com/example/demo/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/demo/domain/post/repository/PostRepository.java
@@ -1,4 +1,20 @@
 package com.example.demo.domain.post.repository;
 
-public interface PostRepository {
+import com.example.demo.domain.post.domain.Post;
+import com.example.demo.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PostRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"writer"})
+    @Query("select m from Post m where m.title = :title")
+    Page<Post> findByTitle(@Param("title") String title, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"writer"})
+    @Query("select m from Post m order by m.view desc")
+    Page<User> findAll(Pageable pageable);
 }
