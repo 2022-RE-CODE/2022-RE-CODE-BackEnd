ed79fb4496abfcf6b7c9f7b41453a10fbaa5f306
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-08-25 15:56:05 +0900

    FEAT :: Comment Repository - Graph (#43)

---
+++ src/main/java/com/example/demo/domain/post/repository/PostCommentRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain.post.repository;
+
+import com.example.demo.domain.post.domain.PostComment;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+    
+    @EntityGraph(attributePaths = {"writer", "post"})
+    @Query("select pc from PostComment pc where pc.post.id = :id order by pc.createdAt")
+    List<PostComment> findAllDesc(@Param("id") Long id);
+
+}
