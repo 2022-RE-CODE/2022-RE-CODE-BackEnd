bc7d24a1fbbf15fbb6e9a162fc2536fe380e1865
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-08-27 16:43:06 +0900

    REFACTOR :: payment

--- src/main/java/com/example/demo/domain/payment/type/PaymentMethod.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.payment.type;
-
-public enum PaymentMethod {
-    CARD,
-    TRANS,
-    VBANK,
-    PHONE
-}
--- src/main/java/com/example/demo/domain/payment/type/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.payment.type;
-
-public enum PaymentStatus {
-    READY,
-    PAID,
-    FAILED,
-    CANCELLED
-}
