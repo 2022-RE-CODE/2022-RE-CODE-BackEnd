d992cca05203527d017ad4dbd627c6b0da1b2d3e
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-10-18 15:47:32 +0900

    REFACTOR :: Service에 있던 HTML Provider로 분리

--- src/main/java/com/example/demo/domain/user/service/EmailService.java
+++ src/main/java/com/example/demo/domain/user/service/EmailService.java
@@ -12,7 +12,6 @@ import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
-import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
@@ -90,25 +89,10 @@ public class EmailService {
         message.addRecipients(Message.RecipientType.TO, email); // 보내는 대상
         message.setSubject("[Re:Code] Confirm " + email + " to sign up"); // 제목
 
-        String msgg = "";
-        msgg += "<h2 style='color:#0068ff;'><strong> Re:Code 회원가입을 위한 이메일 주소를 확인해주세요. </strong></h2>";
-        msgg += "<span style='font-size:17px ;'>반가워요! " + email
-                + "</span><span style='font-size:17px';>을 통한"
-                + " <strong>Re:Code</strong></span>"
-                + "<span style='font-size:17px';>의 요청을 확인 중입니다.</span>" +
-                "<p style='font-size:17px ;'>인증 절차가 성공적으로 이루어지면</p>" + keyword + "을 빠르게 도와드릴게요.";
-        msgg += "<p style='font-size:17px ;'><strong>인증 코드를 웹사이트에 동일하게 작성해주세요: </strong></p>";
-        msgg += "<div align='center' font-family:verdana';>";
-        msgg += "<div style='font-size:200%'><strong>";
-        msgg += ePw + "</strong><div><br/> ";
-        msgg += "</div>";
-        msgg += "<div align='left'><p style='font-size:17px';><strong>인증을 진행한 적이 없으시다면?</strong></p>";
-        msgg += "<p style='font-size:17px';>걱정하지 마세요! 이메일 주소가 타인에 의해 잘못 기입된 것으로 보입니다.</p>";
-        msgg += "<p style='font-size:17px';>타인에게 인증 코드를 알려주지 않도록, 해당 이메일을 무시하거나 삭제하셔도 됩니다.</p></div>";
-        message.setText(msgg, "utf-8", "html"); // 내용
+        String msg = Provider.contentHtml(email, keyword);
+        message.setText(msg, "utf-8", "html"); // 내용
         message.setFrom(new InternetAddress("rltgjqmduftlagl@gmail.com", "Re:Code"));// 보내는 사람
 
-        msgg += "<div style='margin:100px;'>";
         return message;
     }
 }
--- src/main/java/com/example/demo/domain/user/service/Provider.java
+++ src/main/java/com/example/demo/domain/user/service/Provider.java
@@ -2,6 +2,8 @@ package com.example.demo.domain.user.service;
 
 import java.util.Random;
 
+import static com.example.demo.domain.user.service.EmailService.ePw;
+
 public class Provider {
     public static String getRandomString() {
         StringBuilder key = new StringBuilder();
@@ -24,5 +26,25 @@ public class Provider {
         }
 
         return key.toString();
+    }
+
+    public static String contentHtml(String email, String keyword) {
+        String msg = "";
+        msg += "<h2 style='color:#0068ff;'><strong> Re:Code 회원가입을 위한 이메일 주소를 확인해주세요. </strong></h2>";
+        msg += "<span style='font-size:17px ;'>반가워요! " + email
+                + "</span><span style='font-size:17px';>을 통한"
+                + " <strong>Re:Code</strong></span>"
+                + "<span style='font-size:17px';>의 요청을 확인 중입니다.</span>" +
+                "<p style='font-size:17px ;'>인증 절차가 성공적으로 이루어지면</p>" + keyword + "을 빠르게 도와드릴게요.";
+        msg += "<p style='font-size:17px ;'><strong>인증 코드를 웹사이트에 동일하게 작성해주세요: </strong></p>";
+        msg += "<div align='center' font-family:verdana';>";
+        msg += "<div style='font-size:200%'><strong>";
+        msg += ePw + "</strong><div><br/> ";
+        msg += "</div>";
+        msg += "<div align='left'><p style='font-size:17px';><strong>인증을 진행한 적이 없으시다면?</strong></p>";
+        msg += "<p style='font-size:17px';>걱정하지 마세요! 이메일 주소가 타인에 의해 잘못 기입된 것으로 보입니다.</p>";
+        msg += "<p style='font-size:17px';>타인에게 인증 코드를 알려주지 않도록, 해당 이메일을 무시하거나 삭제하셔도 됩니다.</p></div>";
+
+        return msg;
     }
 }
